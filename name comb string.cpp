d1ae725cb9ab183d4004ceb53ff285fabd420dfb
RobaheSepid <robahe.sepid.p@gmail.com>
2024-12-17 22:39:44 +0330

    simple app for calculate two name combination and length

---
+++ name comb string.cpp
@@ -0,0 +1,21 @@
+#include <iostream> // this simple app for calculate two name lenght and combination it
+#include <string>
+using namespace std;
+
+int main(){
+	int a;
+	string name1,name2,namecomb;
+	cout << "enter name 1 space name 2: ";
+	cin >> name1 >> name2;
+    namecomb = name1 + name2;
+	cout << "enter 1 for show name combination,enter 2 for show length of name combination: "<< endl;
+	cin >> a;
+	switch (a){
+		case 1 : cout << "name combination is : " << namecomb;
+		        break;
+		case 2 : cout << "name combination length is: " << namecomb.size();
+		        break;
+		default : cout <<"wrong number";
+	}
+	return 0;
+}
